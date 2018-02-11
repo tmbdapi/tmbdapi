e2eba97f0ddfbfb4c2c03f6dd0702539e2c3c0c8
tmbdapi <tmbdapi@gmail.com>
2018-02-11 21:27:09 +0600

    init project

---
+++ src/main/java/com/bd/tm/TutorMediaApiApplication.java
@@ -0,0 +1,12 @@
+package com.bd.tm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorMediaApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bd/tm/TutorMediaApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bd.tm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TutorMediaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
